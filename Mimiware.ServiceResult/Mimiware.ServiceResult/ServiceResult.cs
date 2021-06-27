e72a78e014fa35c8e2a499d39208a5745c0c2f07
Mikael Johansson <the.mikael.johansson@gmail.com>
2021-06-27 22:20:51 +0200

    feat: add more service result codes

--- Mimiware.ServiceResult/Mimiware.ServiceResult/ServiceResult.cs
+++ Mimiware.ServiceResult/Mimiware.ServiceResult/ServiceResult.cs
@@ -141,8 +141,14 @@
     {
         public const int Ok = 200;
         public const int OkCreated = 201;
+        public const int OkNoContent = 204;
+        public const int BadRequest = 400;
         public const int UnAuthorized = 401;
+        public const int Forbidden = 403;
         public const int NotFound = 404;
+        public const int Conflict = 409;
+        public const int Gone = 410;
         public const int InternalError = 500;
+        public const int ServiceUnavailable = 503;
     }
 }
