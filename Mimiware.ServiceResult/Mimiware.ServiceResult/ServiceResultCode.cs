731486717b03c9e04f231c984a753a9d4a4b486c
Mikael Johansson <mikael.johansson@wolfberry.se>
2023-03-14 18:52:52 +0100

    chore: add more service result codes

--- Mimiware.ServiceResult/Mimiware.ServiceResult/ServiceResultCode.cs
+++ Mimiware.ServiceResult/Mimiware.ServiceResult/ServiceResultCode.cs
@@ -11,8 +11,10 @@ namespace Mimiware.ServiceResult
         public const int OkNoContent = 204;
         public const int BadRequest = 400;
         public const int UnAuthorized = 401;
+        public const int PaymentRequired = 402;
         public const int Forbidden = 403;
         public const int NotFound = 404;
+        public const int MethodNotAllowed = 405;
         public const int Conflict = 409;
         public const int Gone = 410;
         public const int InternalError = 500;
