c3ced8500658d2d02e65021ef865b3d52c5e2d57
Mikael Johansson <mikael.johansson@wolfberry.se>
2024-01-22 21:01:21 +0100

    chore: add .NET 8 example

---
+++ Examples/Example.Net8.RestApi.Backend/Example.Net8.RestApi.Backend/Factories/UsersViewModelFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Example.Net8.RestApi.Backend.Models;
+using Example.Net8.RestApi.Backend.ViewModels;
+
+namespace Example.Net8.RestApi.Backend.Factories;
+
+public static class UsersViewModelFactory
+{
+    public static UsersViewModel Create(List<User> users)
+    {
+        var response = new UsersViewModel
+        {
+            Users = users.Select(x => new UserViewModel
+            {
+                Id = x.Id,
+                Email = x.Email
+            }).ToList()
+        };
+        return response;
+    }
+}
---
+++ Examples/Example.Net8.RestApi.Backend/Example.Net8.RestApi.Backend/Startup.cs
@@ -0,0 +1,46 @@
+using Example.Net8.RestApi.Backend.Repositories;
+using Example.Net8.RestApi.Backend.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Example.Net8.RestApi.Backend;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient<IUserRepository, UserRepository>();
+        services.AddTransient<IUserService, UserService>();
+
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
